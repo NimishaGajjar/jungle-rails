b7808dc424d691e3db71b9f509ea9f021324177e
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-26 19:24:39 -0400

    complete about page. Update navbar and bottom nav

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,10 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  #resources :products, only: [:index, :show]
+  get '/about', to: 'about#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
