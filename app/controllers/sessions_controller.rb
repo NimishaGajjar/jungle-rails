bd3e9121f34c0e70823159f5171de34feb480dc6
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-31 22:22:50 -0400

    Add User model specs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,24 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    user = User.find_by(email: params[:email])
-
-    if user && user.authenticate(params[:password])
-
+    # If the user exists AND the password entered is correct.
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
       session[:user_id] = user.id
-      redirect_to '/', notice: 'Logged in successfully.'
+      redirect_to '/'
     else
-
-      redirect_to '/login', notice: 'Invalid email or password.'
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+    end
   end
-end
 
   def destroy
-    session.delete(:user_id)
-    redirect_to '/login', notice: 'Logged out successfully.'
+    session[:user_id] = nil
+    redirect_to '/login'
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
-  attr_accessor :password_confirmation
-
-  validates :first_name, :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, confirmation: true, length: { minimum: 6 }
-
   has_secure_password
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  # authenticate_with_credentials
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+  
 end
 
